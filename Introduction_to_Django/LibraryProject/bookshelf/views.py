64940f2dc728fe28a542b01743800d911c9bd429
Carine-69 <umugabecarine@gmail.com>
2024-11-14 21:51:36 +0200

    #

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
+# bookshelf/views.py
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def home(request):
+    # Fetch all books from the database
+    books = Book.objects.all()
+    # Pass the list of books to the template
+    return render(request, 'home.html', {'books': books})
