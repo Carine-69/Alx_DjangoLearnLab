c5e95f7a6ef6fc18c83f0d426b38659e206bd874
Carine-69 <umugabecarine@gmail.com>
2024-12-28 20:47:50 +0200

    customised

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,6 +5,6 @@ form . import views
 urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-	path('registration/', views.registration, name='registration'),
-	pth('profile', views.profile, name='profile'),
+	path('register/', views.registration, name='register'),
+	pth('profile/', views.profile, name='profile'),
 ]
