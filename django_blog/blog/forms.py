04f7db7d88924bfb458e0565360348abd9f221b7
Carine-69 <umugabecarine@gmail.com>
2024-12-29 21:58:11 +0200

    few updates

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,49 +1,39 @@
-from django.db.models import Q
-from .models import comment
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.models import User
-from .models import Post, Tag
+from django.contrib.auth.models import User  # Correct import for the User model
+from .models import Comment, Post, Tag  # Correct import for your models
 
+# Form for User creation
 class CreationForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)
 
-	class Meta:
-		model=User
-		fields = ('username','email','password1','password2')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
+# Form for Comment creation
 class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ['content']
+    class Meta:
+        model = Comment
+        fields = ['content']
 
+# Form for Post creation/editing, including tags
 class TagForm(forms.ModelForm):
-	class Meta:
-		model = Post
-		fields = ['title','content', 'tags']
-	tags = forms.ModelMultipleChoiceField(queryset=tag.objects.all(), required=False)
+    class Meta:
+        model = Post  # Correct 'model' to 'Post'
+        fields = ['title', 'content', 'tags']  # Match fields with the Post model
 
+    # Use ModelMultipleChoiceField to allow selecting multiple tags
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False)
 
+    def save(self, commit=True):
+        post = super().save(commit=False)  # Save post object first
+        post.save()  # Save the post to the database
 
-	def save(self, commit=True):
-    		post = super().save(commit=False)
-    		post.save()
+        # Add the tags to the post
+        tags = self.cleaned_data['tags']
+        for tag in tags:
+            post.tags.add(tag)  # Associate each selected tag with the post
 
-    	# Add tags to the post
-    		tags = self.cleaned_data['tags']
-    		for tag in tags:
-        		post.tags.add(tag)
-    
-    		return post
-	
-def search(request):
-    query = request.GET.get('q', '')
-    if query:
-        posts = Post.objects.filter(
-            Q(title__icontains=query) | 
-            Q(content__icontains=query) | 
-            Q(tags__name__icontains=query)
-        ).distinct()
-    else:
-        posts = Post.objects.all()
-    return render(request, 'blog/templates/blog/search_results.html', {'posts': posts, 'query': query})
+        return post
+
