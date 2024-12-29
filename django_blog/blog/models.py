152cadf83ec1c0890f0327bcb0db4a3f53f0118b
Carine-69 <umugabecarine@gmail.com>
2024-12-29 19:19:08 +0200

    commenting possible

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import post
+
 # Create your models here.
 
-class post(models.Model):
-	title=models.CharField(max_length=200)
-	content=models.TextField()
-	published_date=models.TimeField(auto_now_add=True)
-	author=models.ForeignKey(User,on_delete=models.CASCADE)
+# Capitalize the class name 'Post'
+class Post(models.Model):  
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.TimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-class comment(models.Model):
-	post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	content = models.TextField()
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+# Comment model with correct foreign key reference
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return f'comment by {self.author} on {self.post}'
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
+
