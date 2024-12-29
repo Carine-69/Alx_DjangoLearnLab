584b4a46378110dca90ee21901e70bee16419c06
Carine-69 <umugabecarine@gmail.com>
2024-12-29 12:14:00 +0200

    crud operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 	path('register/', views.registration, name='register'),
-	pth('profile/', views.profile, name='profile'),
+	path('profile/', views.profile, name='profile'),
+	path('', views.PostListView.as_view(), name='post_list'),
+    	path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    	path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
+    	path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    	path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,11 @@
-from djnago.contrib import render, redirect
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import post
+from django.contrib import render, redirect
 from .form import registration
 from django.contrib import messages
+from .forms import postform
+from django.urls import reverse_lazy
 
 def registration(request):
 	if request.method == 'POST':
@@ -22,3 +27,47 @@ def profile(request):
 	else:
 		form = UserRegistration(instance=request.user)
 	return render(request, 'blog/templates/profile.html',{'form':form})
+
+
+class postListView(ListView):
+	model = post
+	name = 'blog/templates/blog/ListView.html'
+	context_object_name = 'posts'
+
+class DetailView():
+	model = post
+	template_name = 'blog/templates/blog/post_detail.html'
+	context_object_name = 'post'
+
+class postCreateView(LoginRequiredMixin, CreateView): 
+	model = post
+	form_class = postform
+	template_name = 'blog/templates/blog/CreateView.html'
+	success_url = reverse_lazy('ListView') # after successful creaton, it redirects to list view
+
+	def form_valid(self, form):
+		from.instance.author = self.request.user # this assign the logged in user as the author of the post
+		return super().form_valid(form)
+
+class postUpdateView(LoginRequiredMixin, UserPassesMixin, UpdateView):
+	method = post
+	form_class = postform
+	template_name = 'blog/templates/blog/updateView.html'
+	
+	def test_func(self):
+	post = self.get.object()
+	return self.request.user == post.author # only author of post can edit it
+
+
+	def get_success_url(self):
+	return reverse_lazy('post_detail', kwaegs={'pk': self.object.pk})
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('post_list')  # Redirect to the post list page after deletion
+
+	def test_func(self):
+    post = self.get_object()
+    return self.request.user == post.author  # Only allow the author to delete the post
+
