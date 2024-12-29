b1fdc3d5ba50c6c1bf208c9c0918b72d5cdb4cff
Carine-69 <umugabecarine@gmail.com>
2024-12-29 19:41:50 +0200

    commenting possible

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
     # Comment URLs
-    path('post/<int:post_id>/comments/new/', views.add_comment, name='add_comment'),  # Add a new comment
+    path('post/<int:pk>/comments/new/', views.add_comment, name='add_comment'),  # Add a new comment
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),  # Update an existing comment
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),  # Delete a comment
 ]
